2fed872b23c3a20ba8fc57179a5bd8963d57f131
Agrawal <pooja.b.agrawal@capgemini.com>
2020-12-25 12:46:31 +0530

    initial commit

--- AirQuality/AirQuality/settings.py
+++ AirQuality/AirQuality/settings.py
@@ -1,7 +1,7 @@
 
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -104,10 +104,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-import os
+
+
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+django_heroku.settings(locals())
